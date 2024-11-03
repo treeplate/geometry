edf5112bde253d2c32e06172d58a5d23af47e8b2
treeplate <tree.plate.moo@gmail.com>
2024-11-02 20:31:53 -0700

    add comments, rays, and angle measures

--- bin/geometry.dart
+++ bin/geometry.dart
@@ -9,7 +9,7 @@ class GeometryIterator implements Iterator {
   GeometryIterator(this._iterator) {
     _iterator.moveNext();
   }
-  
+
   int get current => _iterator.current;
   bool moveNext() => _iterator.moveNext();
 
@@ -37,34 +37,52 @@ class Thing {
   factory Thing.parse(GeometryIterator iterator) {
     int char1 = iterator.current;
     if (char1 >= 65 && char1 <= 90) {
+      // capital letters
       Point p1 = iterator.getPoint();
       int char2 = iterator.current;
       if (char2 >= 65 && char2 <= 90) {
+        // capital letters
         Point p2 = iterator.getPoint();
         return ValueOf(LineSegment(p1, p2));
-      } else {
-        return p1;
       }
+      return p1;
     }
     if (char1 == 45) {
+      // -
       iterator.moveNext();
       Point a = iterator.getPoint();
       Point b = iterator.getPoint();
-      if (iterator.getCharacter() != 45) {
-        throw FormatException('- expected');
+      switch (iterator.getCharacter()) {
+        case 45: // -
+          return LineSegment(a, b);
+        case 62: // >
+          return Ray(a, b);
+        default:
+          throw FormatException('Unexpected character after "-$a$b"');
       }
-      return LineSegment(a, b);
     }
     if (char1 == 60) {
+      // <
       iterator.moveNext();
       Point a = iterator.getPoint();
       Point b = iterator.getPoint();
       if (iterator.current == 62) {
+        // >
         iterator.moveNext();
-        return Line(a,b);
-      } else {
+        return Line(a, b);
+      }
+      Point c = iterator.getPoint();
+      return Angle(a, b, c);
+    }
+    if (char1 == 109) {
+      // m
+      if (char1 == 60) {
+        // <
+        iterator.moveNext();
+        Point a = iterator.getPoint();
+        Point b = iterator.getPoint();
         Point c = iterator.getPoint();
-        return Angle(a,b,c);
+        return ValueOf(Angle(a, b, c));
       }
     }
     throw UnimplementedError();
@@ -75,9 +93,7 @@ class Point extends Thing {
   final int name;
   final int primes;
   const Point(this.name, this.primes);
- 
-  operator ==(Object? other) => other is Point && other.name == name;
-  int get hashCode => name.hashCode;
+
   String toString() => String.fromCharCode(name) + '\'' * primes;
 }
 
@@ -90,9 +106,15 @@ class LineSegment extends Measurable {
   final Point b;
   const LineSegment(this.a, this.b);
 
-  operator ==(Object? other) => other is LineSegment && (other.a == a && other.b == b || other.b == a && other.a == b);
-  int get hashCode => a.hashCode ^ b.hashCode;  
   String toString() => 'segment $a$b';
+}
+
+class Ray extends Thing {
+  final Point a; // endpoint
+  final Point b;
+  const Ray(this.a, this.b);
+
+  String toString() => 'ray $a$b->';
 }
 
 class Line extends Thing {
@@ -100,20 +122,15 @@ class Line extends Thing {
   final Point b;
   const Line(this.a, this.b);
 
-  operator ==(Object? other) => other is Line && (other.a == a && other.b == b || other.b == a && other.a == b);
-  int get hashCode => a.hashCode ^ b.hashCode;  
   String toString() => 'line <-$a$b->';
-
 }
 
-class Angle extends Thing {
+class Angle extends Measurable {
   final Point a;
   final Point b;
   final Point c;
   const Angle(this.a, this.b, this.c);
 
-  operator ==(Object? other) => other is Angle && other.b == b && (other.a == a && other.b == b || other.b == a && other.a == b);
-  int get hashCode => b.hashCode * (a.hashCode ^ c.hashCode);  
   String toString() => '∠$a$b$c';
 }
 
@@ -122,7 +139,7 @@ class ValueOf extends Thing {
   const ValueOf(this.thing);
 
   String toString() {
-    switch(thing) {
+    switch (thing) {
       case LineSegment(a: Point a, b: Point b):
         return '$a$b';
       case Angle(a: Point a, b: Point b, c: Point c):
